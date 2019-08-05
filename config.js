e6361564c25359679b12333ce25bd27b4c04cf1a
Victor Juan <vj.jimenez96@gmail.com>
2019-08-04 21:04:18 -0500

    mi first bot

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: "",
+    FB_PAGE_TOKEN: 'EAALJhOr7qh4BAFOM51tfXVnrUAyVOMIzazoPOZA7u38tSFeg5VlHKnsLDc1iwBKY9XTkWcmB2MA2gxMI4qw2Gb5Fm1NSZCHyDj1M273nFsFBil3g4GTmPKXQyiNmZCdJIekXRaU1jjI0HidjU1aXq7LyDRphpGo5W1cNgqUn1D3twmurBqy',
+    FB_VERIFY_TOKEN: 'testbot',
+    API_AI_CLIENT_ACCESS_TOKEN: 'ad73fb7e8b3042fb9097b7e82e7928f2',
+    FB_APP_SECRET: '21183e0b6e47ca1e3a24daa1713ce771',
+    SERVER_URL: "https://charlybotheroku.herokuapp.com/webhook",
 };
